1f15d48e5fc3f910ebc0f4dd042357738e5ca3fd
Abdirahmaan <Abdirahmaan874@gmail.com>
2024-12-12 14:32:42 +0300

    first angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent   {
+  title = "ff";
+  nm : string = '';
+  em : string = '';
+  ph : string= '';
+  add : string = '';
+ showHeading : boolean = false;
+  qualifications = [{school: '', degree:'', year :''}];
+  submitted : boolean = false
+
+  addQualification(){
+    this.qualifications.push({school: '',degree: "", year:""})
+  }
+
+  submit() {
+    this.submitted = true;
+    this.showHeading =  false;
+  }
+  edit() {
+    this.submitted = false;
+    this.showHeading =  true;
+  }
+
 }
+
+
+
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent {
+
+ calc = {
+   name : "Abdirahman",
+   age  : 24,
+   location  :"new york"
+ }
+
+
+ }
+ 
+
+
